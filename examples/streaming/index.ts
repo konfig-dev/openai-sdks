b6b2ac2dc391a6804d9b3658f17e32e4fc9d0f06
dphuang2 <dylan.p.huang@gmail.com>
2023-03-31 18:00:48 -0700

    sav

--- examples/streaming/index.ts
+++ examples/streaming/index.ts
@@ -1,1 +1,9 @@
 import { Openai } from "openai-typescript-sdk";
+
+const input = {
+  model: { id: "gpt-3.5-turbo", name: "GPT-3.5" },
+  systemPrompt:
+    "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.",
+  key: "",
+  messages: [{ role: "user", content: "test" }],
+};
